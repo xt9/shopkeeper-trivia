c4e3f70d630d1265991f687fba90e6c4b9d281b5
xt9 <niklas_andersson90@hotmail.com>
2019-11-17 23:22:37 +0100

    Removed early experimentation.

--- src/store/index.js
+++ src/store/index.js
@@ -1,30 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-import Person from "./Person";
-import Pets from "./Pets";
-
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    ...Person.state,
-    ...Pets.state
-  },
-  getters: {
-    ...Person.getters,
-    ...Pets.getters
-  },
-  mutations: {
-    ...Person.mutations,
-    ...Pets.mutations
-  },
-  actions: {
-    ...Person.actions,
-    ...Pets.actions
-  },
-  modules: {
-    ...Person.modules,
-    ...Pets.modules
-  },
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {},
 });
